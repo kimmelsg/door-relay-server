e9628b9070609d1958be3ecdfec2552d4cf87c6c
Ethan Knowlton <eknowlton@gmail.com>
2018-07-03 14:32:03 -0400

    added server

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ source "https://rubygems.org"
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 gem 'rubyserial'
+gem 'rack'
+gem 'pry-byebug'
+gem 'activesupport'
---
+++ server.rb
@@ -0,0 +1,26 @@
+require 'rack'
+require 'json'
+require 'pry-byebug'
+require 'active_support/all'
+
+require './relay'
+
+relay = Relay.new('/dev/ttyUSB0')
+
+def valid_request?(params)
+  return false unless (1..8).cover? params[:relay].to_i
+  return false unless %w[on off].include? params[:status]
+  true
+end
+
+app = proc do |env|
+  request = Rack::Request.new(env)
+  params = request.params.symbolize_keys
+  unless valid_request?(params)
+    next ['400', { 'Content-Type' => 'text/html'}, ['Invalid Request']]
+  end
+  result = relay.send params[:status], params[:relay]
+  ['200', { 'Content-Type' => 'text/html' }, [result]]
+end
+
+Rack::Handler::WEBrick.run app
