2416b35995b4f2ef0630f2a4cf79df86fc6e6d5f
Ethan Knowlton <eknowlton@gmail.com>
2018-07-03 12:28:38 -0400

    clean up code

--- relay.rb
+++ relay.rb
@@ -10,6 +10,8 @@ class Relay
   end
 
   # turn on a relay
+  # @param [Integer] relay Index of relay to operate on.
+  # @return [Boolean] Success?
   def on(relay)
     lsb = relay - 1 & 255
     msb = relay >> 8
@@ -18,6 +20,9 @@ class Relay
     process_control_command_return(result)
   end
 
+  # turn off a relay
+  # @param [Integer] relay Index of relay to operate on.
+  # @return [Boolean] Success?
   def off(relay)
     lsb = relay - 1 & 255
     msb = relay >> 8
@@ -30,8 +35,8 @@ class Relay
 
   attr_reader :serial
 
-  # verify data is setup for controller to process
-  # data:[]
+  # @param [Array<Integer>] data A sequence of data.
+  # @return [Array<Integer>] data A sequence of data, API wrapped, w/ checksum.
   def wrap_in_api(data)
     bytes_in_packet = data.length
     data = data.unshift(bytes_in_packet)
@@ -39,16 +44,15 @@ class Relay
     add_checksum(data)
   end
 
-  # add checksum to the data
-  # data:[]
+  # @param [Array<Integer>] data A sequence of data.
+  # @return [Array<Integer>] data A sequence of data, with appended checksum.
   def add_checksum(data)
     data << (data.inject(0) { |sum, x| sum + x } & 255)
   end
 
-  # send command to serial device
-  # command:[]
-  # return_bytes_length:int
-  # @return string
+  # @param [Array<Integer>] command A sequence of data.
+  # @param [Integer] return_bytes_length A length of bytes to expect back.
+  # @return [String]
   def send_command(command, return_bytes_length = 4)
     command = convert_data(command) # command:string
     serial.write(command) # command:string
@@ -56,8 +60,8 @@ class Relay
   end
 
   # Convert data into bit/bytes?
-  # command:[]
-  # @return string
+  # @param [Array<Integer>] command A sequence of data.
+  # @return [String] Converted bytes to bits
   def convert_data(command)
     converted = ''
     command.each do |c|
@@ -66,15 +70,15 @@ class Relay
     converted
   end
 
-  # determine validity of control_command
-  # data:string
+  # @params [String] data String response from serial.
+  # @return [Boolean] Success?
   def process_control_command_return(data)
     return false unless valid?(data)
     true
   end
 
-  # grouped validation
-  # data:string
+  # @params [String] data String response from serial.
+  # @return [Boolean] Sucess?
   def valid?(data)
     return false unless valid_handshake?(data)
     return false unless valid_bytes_back?(data)
@@ -82,32 +86,24 @@ class Relay
     true
   end
 
-  # Recieving data from controller
-  def get_payload(data)
-    payload = []
-    sub_length = data.length - 1
-    (2..sub_length).each do |byte|
-      payload << data[byte, byte + 1].ord
-    end
-    payload
-  end
-
-  # Validation of Data
-  # data:string
+  # @params [String] data String response from serial.
+  # @return [Boolean] Success?
   def valid_handshake?(data)
     puts 'Handshake Data: '
     puts data
     data[0, 1].ord == 170
   end
 
-  # data:string
+  # @params [String] data String response from serial.
+  # @return [Boolean] Success?
   def valid_bytes_back?(data)
     puts 'Valid Bytes Back: '
     puts data
     data[1, 2].ord == (data.length - 3)
   end
 
-  # data:string
+  # @params [String] data String response from serial.
+  # @return [Boolean] Success?
   def valid_checksum?(data)
     puts 'Valid Checksum: '
     put data
